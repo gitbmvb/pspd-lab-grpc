f6dd0a441520a4c7798d99b31aa29c5a9cab6629
gitbmvb <bruno.martval@gmail.com>
2025-04-30 20:37:43 -0300

    feat: área de chat

---
+++ frontend/src/components/Main/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>PSPD Labs</p>
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Olá, FW Cruz.</span></p>
+                    <p>Como posso te ajudar hoje?</p>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Pergunte alguma coisa' />
+                        <div>
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">UnB/FGA@2025.1</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
