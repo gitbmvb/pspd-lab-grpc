387b5e0da020da43dfcb611b8d49fcdf7cb23422
gitbmvb <bruno.martval@gmail.com>
2025-04-30 21:49:45 -0300

    feat: tela de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/Main'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
-    <>
-      <Sidebar />
-      <Main />
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/main"
+          element={
+            <>
+              <Sidebar />
+              <Main />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Main.css'
-import { assets } from '../../assets/assets'
+import { assets } from '../assets/assets'
 
 const Main = () => {
     return (
